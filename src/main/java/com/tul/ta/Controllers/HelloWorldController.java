0ff970e47869ce756b15737bb9f04a8bb9f5215f
Kaszuba-203900 <37172821+Kaszuba-203900@users.noreply.github.com>
2018-04-23 12:36:18 +0200

    Initial commit

---
+++ src/main/java/com/tul/ta/Controllers/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.tul.ta.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from my app";
+    }
+}

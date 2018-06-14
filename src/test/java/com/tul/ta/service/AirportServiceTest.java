31db0b8637a038ad67fab99de58ded8140b3f971
Mielczarek-203943 <kuba.mielczarek96@o2.pl>
2018-06-14 08:52:10 +0200

    Add tests

--- src/test/java/com/tul/ta/service/AirportServiceTest.java
+++ src/test/java/com/tul/ta/service/AirportServiceTest.java
@@ -5,14 +5,19 @@ import com.tul.ta.model.airport.Airport;
 import com.tul.ta.repository.AirportRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 @RunWith(SpringRunner.class)
 public class AirportServiceTest {
@@ -27,10 +32,15 @@ public class AirportServiceTest {
     }
 
     @MockBean
-    private AirportRepository airportRepository;
+    private AirportRepository airportRepositoryMock;
 
     @Autowired
     private AirportService airportService;
+
+    @Test
+    public void serviceClasspathShouldBeCorrect() {
+        assertEquals("class com.tul.ta.service.DefaultAirportService", airportService.getClass().toString() );
+    }
 
     @Test(expected = ResourceNotFoundException.class)
     public void whenValidAirportCodeThenAirportShouldBeFound() {
@@ -42,7 +52,7 @@ public class AirportServiceTest {
                 .timeZoneId("European/Copenhagen")
                 .build();
 
-        Mockito.when(airportRepository.findById(AAL.getAirportCode())
+        when(airportRepositoryMock.findById(AAL.getAirportCode())
                 .orElseThrow(() -> new ResourceNotFoundException("Airport", "id", "AAL")))
                 .thenReturn(AAL);
 
@@ -50,5 +60,29 @@ public class AirportServiceTest {
         Airport found = airportService.getAirportById(airportCode);
 
         assertEquals(found.getAirportCode(), airportCode);
+    }
+
+    @Test
+    public void findAllShouldReturnListOfAirportEntries() {
+        List<Airport> airports = new ArrayList<>();
+        when(airportRepositoryMock.findAll()).thenReturn(airports);
+
+        List<Airport> actual = airportService.getAll();
+
+        verify(airportRepositoryMock, times(1)).findAll();
+        verifyNoMoreInteractions(airportRepositoryMock);
+
+        assertThat(actual, is(airports));
+    }
+
+    @Test(expected = ResourceNotFoundException.class)
+    public void findAirportByIdShouldThrowNotFoundException() throws ResourceNotFoundException {
+        String airportCode = "AAL";
+        when(airportRepositoryMock.findById(airportCode)).thenThrow(new ResourceNotFoundException("Airport", "id", airportCode));
+
+        airportService.getAirportById(airportCode);
+
+        verify(airportRepositoryMock, times(1)).findById(airportCode);
+        verifyNoMoreInteractions(airportRepositoryMock);
     }
 }

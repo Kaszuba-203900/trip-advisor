d29eaa0603326cb224d3913cfbd8ff7b13c40b82
pl_marcin <203947@edu.p.lodz.pl>
2018-06-20 00:29:13 +0200

    Added Ticket class with fields for database

---
+++ src/main/java/com/tul/ta/model/Ticket.java
@@ -0,0 +1,44 @@
+package com.tul.ta.model;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Tolerate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@EqualsAndHashCode()
+@Data
+@Table(name = "tickets")
+public class Ticket {
+    @Id
+    @Column(name = "ticket_id")
+    public Integer ticketId;
+    @Column(name = "from_city")
+    public String fromCityName;
+    @Column(name = "to_city")
+    public String toCityName;
+    @Column(name = "date_of_flight")
+    public Date dateOfFlight;
+    @Column(name = "price")
+    public Double price;
+
+    @Builder
+    public Ticket(Integer ticketId, String fromCityName, String toCityName, Date dateOfFlight, Double price) {
+        this.ticketId = ticketId;
+        this.fromCityName = fromCityName;
+        this.toCityName = toCityName;
+        this.dateOfFlight = dateOfFlight;
+        this.price = price;
+    }
+
+    @Tolerate
+    public Ticket(){
+        
+    }
+}

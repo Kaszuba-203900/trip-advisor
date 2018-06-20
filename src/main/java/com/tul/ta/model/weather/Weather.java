5ad23be5839ea419138ec789acf390963cabb8b9
kaszuba <bartek.kaszuba4444@wp.pl>
2018-06-20 14:49:03 +0200

    Add lombok for Weather.class, fixed pom and properties

--- src/main/java/com/tul/ta/model/weather/Weather.java
+++ src/main/java/com/tul/ta/model/weather/Weather.java
@@ -1,8 +1,11 @@
 package com.tul.ta.model.weather;
 
+import lombok.Data;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+@Data
 public class Weather {
 
     private int lowTemperature;
@@ -11,54 +14,11 @@ public class Weather {
     private String date;
     private String city;
 
-    public Weather() {
-    }
-
     public Weather(int lowTemperature, int highTemperature, String description, Date date, String city) {
         this.lowTemperature = lowTemperature;
         this.highTemperature = highTemperature;
         this.description = description;
         this.date = new SimpleDateFormat("yyyy-MM-dd").format(date);
-        this.city = city;
-    }
-
-    public int getLowTemperature() {
-        return lowTemperature;
-    }
-
-    public void setLowTemperature(int lowTemperature) {
-        this.lowTemperature = lowTemperature;
-    }
-
-    public int getHighTemperature() {
-        return highTemperature;
-    }
-
-    public void setHighTemperature(int highTemperature) {
-        this.highTemperature = highTemperature;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
         this.city = city;
     }
 

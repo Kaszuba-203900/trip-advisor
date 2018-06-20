ca53e36ff5cfee738190b9e51bcff873f322fc4e
Mlodzik-203947 <chlodzik14@interia.pl>
2018-06-20 22:09:32 +0200

    Added working post and get methods for tickets and implemented all methods to save / load tickets

--- src/main/java/com/tul/ta/repository/TicketRepository.java
+++ src/main/java/com/tul/ta/repository/TicketRepository.java
@@ -1,4 +1,11 @@
 package com.tul.ta.repository;
 
-public interface TicketRepository {
+import com.tul.ta.model.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
 }
---
+++ src/main/java/com/tul/ta/service/DefaultTicketService.java
@@ -0,0 +1,27 @@
+package com.tul.ta.service;
+
+
+import com.tul.ta.model.Ticket;
+import com.tul.ta.repository.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class DefaultTicketService implements TicketService {
+    @Autowired
+    TicketRepository ticketRepository;
+
+    @Override
+    public Ticket save (final Ticket ticket){
+        return ticketRepository.save(ticket);
+    }
+
+    @Override
+    public List<Ticket> findAll(){
+        return ticketRepository.findAll();
+    }
+
+}
